abd27f67656855b8dc46975ca66c0a29a36880bf
Alex Cioarec <cioarec.alexandru96@gmail.com>
2024-02-15 04:52:41 +0200

    Progress on hero section

--- src/components/HeroComponent.vue
+++ src/components/HeroComponent.vue
@@ -1,7 +1,28 @@
 <template>
-  <div class="hero-container grid grid-cols-1 sm:grid-cols-12">
-    <div class="bg-red-400 opacity-50 sm:col-span-7">Text</div>
-    <div class="bg-blue-400 opacity-50 sm:col-span-5">Video</div>
+  <div class="hero-container grid grid-cols-1 sm:grid-cols-12 overflow-hidden">
+    <div class="background-image"></div>
+    <div class="sm:col-span-7 z-10">
+      <!-- <div class="px-5 sm:px-10 md:px-20 lg:px-28"> -->
+      <div class="flex justify-center items-center">
+        <div class="flex flex-col justify-center sm:px-10">
+          <img
+            class="w-24 md:w-32 lg:w-40 mt-48 sm:mt-72 lg:mt-72"
+            src="@/assets/images/usr-logo.png"
+          />
+          <img
+            class="mt-8 max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg xl:max-w-2xl"
+            src="@/assets/images/nume.png"
+          />
+          <img
+            class="mt-3 max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg xl:max-w-2xl"
+            src="@/assets/images/subtext.png"
+          />
+        </div>
+      </div>
+    </div>
+    <div class="sm:col-span-5 z-10 flex justify-center items-center min-h-[500px]">
+      <img class="w-28 h-28" src="@/assets/images/play.png" />
+    </div>
   </div>
 </template>
 
@@ -10,11 +31,38 @@
 <style>
 .hero-container {
   min-height: 800px;
+  position: relative;
+  z-index: 0;
+}
+
+.background-image {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
   background-image: url('../assets//images//primarie.png');
   background-size: cover;
   background-position: center;
   filter: blur(10px);
-  z-index: 0;
+}
+
+.m-name {
+  font-family: Montserrat;
+  font-weight: 900;
+  line-height: 96px;
+  letter-spacing: 0em;
+  text-align: left;
+  color: #002a59;
+}
+
+.m-sub {
+  font-family: Montserrat;
+  font-weight: 900;
+  line-height: 48px;
+  letter-spacing: 0em;
+  text-align: left;
+  color: #ff0021;
 }
 
 .hero-container::before {
